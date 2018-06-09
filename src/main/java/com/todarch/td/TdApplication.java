524c6cb4938c2e2efac6969536c6e8e1d949ccd3
selimssevgi <selimssevgi@gmail.com>
2018-06-09 15:00:22 +0300

    init project

---
+++ src/main/java/com/todarch/td/TdApplication.java
@@ -0,0 +1,12 @@
+package com.todarch.td;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TdApplication.class, args);
+  }
+}
---
+++ src/test/java/com/todarch/td/TdApplicationTests.java
@@ -0,0 +1,14 @@
+package com.todarch.td;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TdApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+}
