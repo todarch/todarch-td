6a661d879216457a3356978e9bc77a4e49ad729b
selimssevgi <selimssevgi@gmail.com>
2018-10-28 20:08:36 +0200

    feat: find tags by todoid on tag repository

--- src/test/java/com/todarch/td/domain/tag/TagRepositoryTest.java
+++ src/test/java/com/todarch/td/domain/tag/TagRepositoryTest.java
@@ -1,18 +1,28 @@
 package com.todarch.td.domain.tag;
 
+import com.todarch.td.domain.shared.Tag;
 import com.todarch.td.helper.RepositoryTest;
+import com.todarch.td.helper.TestTag;
+import com.todarch.td.helper.TestTodo;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.PersistenceException;
+
+import java.util.List;
 
 import static org.junit.Assert.fail;
 
 public class TagRepositoryTest extends RepositoryTest {
 
+  @Autowired
+  private TagRepository tagRepository;
+
   @Test
   public void userIdAndNameTogetherShouldBeUnique() {
-    Tag tag = new Tag(5L, "Tag1");
-    Tag sameTag = new Tag(5L, "Tag1");
+    TagEntity tag = new TagEntity(TestTag.ID,5L, TestTag.VALUE);
+    TagEntity sameTag = new TagEntity(TestTag.ID, 5L, TestTag.VALUE);
     tem.persistAndFlush(tag);
     try {
       tem.persistAndFlush(sameTag);
@@ -20,5 +30,37 @@ public class TagRepositoryTest extends RepositoryTest {
     } catch (PersistenceException ignore) {
       //
     }
+  }
+
+  @Test
+  public void shouldFindAllByTodoId() {
+    var nonExistingTodoTags = tagRepository.findAllByTodoId(TestTodo.nextId());
+    Assertions.assertThat(nonExistingTodoTags).isEmpty();
+
+    var tagEntity = new TagEntity(TestTag.ID,5L, TestTag.VALUE);
+    tagEntity.assignTo(TestTodo.ID);
+    tem.persistFlushFind(tagEntity);
+
+    var anotherUnrelatedTagEntity = new TagEntity(TestTag.nextId(),6L, Tag.of("Tag3"));
+    anotherUnrelatedTagEntity.assignTo(TestTodo.nextId());
+    tem.persistFlushFind(anotherUnrelatedTagEntity);
+
+    var allTagsOfTestTodo = tagRepository.findAllByTodoId(TestTodo.ID);
+    Assertions.assertThat(allTagsOfTestTodo).hasSize(1);
+    Assertions.assertThat(allTagsOfTestTodo.get(0).id()).isEqualTo(tagEntity.id());
+  }
+
+  @Test
+  public void shouldRemoveUnassignedTag() {
+    var tagEntity = new TagEntity(TestTag.ID,5L, TestTag.VALUE);
+    tagEntity.assignTo(TestTodo.ID);
+    tem.persistFlushFind(tagEntity);
+
+    var sameTagEntity = tagRepository.findById(tagEntity.id()).get();
+    sameTagEntity.unassignFrom(TestTodo.ID);
+    tem.persistFlushFind(sameTagEntity);
+
+    var finalTagEntity = tagRepository.findById(tagEntity.id()).get();
+    Assertions.assertThat(finalTagEntity.taggedTodos()).isEmpty();
   }
 }
